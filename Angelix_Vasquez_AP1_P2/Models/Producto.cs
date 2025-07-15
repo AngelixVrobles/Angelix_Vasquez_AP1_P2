49d73d4a984ee2a9dc0bc3ed72685e1d459a27c9
Estudiantesucne <Estudiantesucne@TERMINAL-B02>
2025-07-14 21:14:21 -0400

    Todo lo otro

---
+++ Angelix_Vasquez_AP1_P2/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Angelix_Vasquez_AP1_P2.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int ProductosId { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; } = string.Empty;
+
+        public double Peso { get; set; }
+
+        public virtual ICollection<EntradasDetalle> EntradasDetalle { get; set; } = new List<EntradasDetalle>();
+    }
+}
