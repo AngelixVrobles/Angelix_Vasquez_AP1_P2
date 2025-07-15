7987960a0f909f6586b931d1e76850b3a6c419dc
Estudiantesucne <Estudiantesucne@TERMINAL-B02>
2025-07-14 21:12:27 -0400

    Creacion de migraciones

---
+++ Angelix_Vasquez_AP1_P2/Migrations/20250715000642_Intial.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Angelix_Vasquez_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entradas",
+                columns: table => new
+                {
+                    EntradasId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Concepto = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entradas", x => x.EntradasId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductosId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Peso = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductosId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntradasDetalle",
+                columns: table => new
+                {
+                    DetallesId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntradasId = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasDetalle", x => x.DetallesId);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalle_Entradas_EntradasId",
+                        column: x => x.EntradasId,
+                        principalTable: "Entradas",
+                        principalColumn: "EntradasId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalle_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductosId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductosId", "Descripcion", "Peso" },
+                values: new object[,]
+                {
+                    { 1, "Mani", 0.0 },
+                    { 2, "Almendra", 0.0 },
+                    { 3, "Pistacho", 0.0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalle_EntradasId",
+                table: "EntradasDetalle",
+                column: "EntradasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalle_ProductoId",
+                table: "EntradasDetalle",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntradasDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Entradas");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
---
+++ Angelix_Vasquez_AP1_P2/Migrations/20250715010640_Cantidad.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Angelix_Vasquez_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cantidad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Cantidad",
+                table: "EntradasDetalle",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "ProductosId",
+                keyValue: 1,
+                column: "Peso",
+                value: 12.0);
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "ProductosId",
+                keyValue: 2,
+                column: "Peso",
+                value: 17.0);
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "ProductosId",
+                keyValue: 3,
+                column: "Peso",
+                value: 3.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cantidad",
+                table: "EntradasDetalle");
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "ProductosId",
+                keyValue: 1,
+                column: "Peso",
+                value: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "ProductosId",
+                keyValue: 2,
+                column: "Peso",
+                value: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Productos",
+                keyColumn: "ProductosId",
+                keyValue: 3,
+                column: "Peso",
+                value: 0.0);
+        }
+    }
+}
